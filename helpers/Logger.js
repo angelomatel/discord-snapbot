bce4d3744e1ae6a63ab03429cd7e14c1104fd537
angelomatel <mateljohnangelo@gmail.com>
2025-05-13 02:02:10 +0800

    Update Logger.js
    Remove custom logger level to avoid duplicates in logs
    Update file logs to include timestamp (properly this time)

--- helpers/Logger.js
+++ helpers/Logger.js
@@ -1,25 +1,6 @@
 const Winston = require('winston');
 
-const customLevels = {
-    levels: {
-        error: 0,
-        warn: 1,
-        info: 2,
-        verbose: 3,
-        debug: 4,
-        silly: 5,
-        item: 99,
-    },
-    colors: {
-        item: 'cyan',
-        info: 'green',
-        error: 'red',
-        warn: 'yellow',
-    },
-}
-
 const Logger = Winston.createLogger({
-    levels: customLevels.levels,
     transports: [
         new Winston.transports.Console({
             level: 'info',
@@ -32,6 +13,7 @@ const Logger = Winston.createLogger({
             filename: 'logs/bot-error.log',
             level: 'error',
             format: Winston.format.combine(
+                Winston.format.timestamp(),
                 Winston.format.simple(),
                 Winston.format.printf(({ timestamp, level, message }) => {
                     return `${timestamp} ${level}: ${message}`;
@@ -42,6 +24,7 @@ const Logger = Winston.createLogger({
             filename: 'logs/bot-info.log',
             level: 'info',
             format: Winston.format.combine(
+                Winston.format.timestamp(),
                 Winston.format.simple(),
                 Winston.format.printf(({ timestamp, level, message }) => {
                     return `${timestamp} ${level}: ${message}`;
@@ -53,28 +36,16 @@ const Logger = Winston.createLogger({
             level: 'warn',
             format: Winston.format.combine(
                 Winston.format.timestamp(),
-                Winston.format.simple()
-            )
-        }),
-        new Winston.transports.File({
-            filename: 'logs/items.log',
-            level: 'item',
-            format: Winston.format.combine(
                 Winston.format.simple(),
-                Winston.format.timestamp(),
+                Winston.format.printf(({ timestamp, level, message }) => {
+                    return `${timestamp} ${level}: ${message}`;
+                })
             )
         }),
-        new Winston.transports.Console({
-            level: 'item',
-            format: Winston.format.combine(
-                Winston.format.colorize(),
-                Winston.format.simple()
-            )
-        })
     ]
 });
-Winston.addColors(customLevels.colors);
-Logger.item = (item, type) => {
+
+const item = (item, type) => {
     const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false });
     const expiryTime = new Date(item.EndTime * 1000).toLocaleTimeString('en-US', { hour12: false });
     
@@ -84,9 +55,20 @@ Logger.item = (item, type) => {
     const itemName = item.FullName.replace('<:Broken:980862802803720242>', '[Broken]');
 
     Logger.log({
-        level: 'item',
+        level: 'info',
         message: `[${currentTime} - ${expiryTime}] ${typeS} | ${price} | ${itemName}`,
     });
 }
 
-module.exports = Logger;
+module.exports = {
+    info: (message) => {
+        Logger.info(message);
+    },
+    error: (message) => {
+        Logger.error(message);
+    },
+    warn: (message) => {
+        Logger.warn(message);
+    },
+    item: item,
+};
